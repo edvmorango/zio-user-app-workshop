5a072d632f4c5fb9b9cd556986b020bdf5eb4694
J. Eduardo V. Morango <edvmorango@gmail.com>
2020-04-18 22:46:55 -0300

    feat: transactor

---
+++ src/main/scala/scaladores/environment/repository/package.scala
@@ -0,0 +1,32 @@
+package scaladores.environment
+
+import doobie.util.transactor.Transactor
+import zio.{Has, Task, ZIO, ZLayer}
+import zio.interop.catz._
+import config._
+
+package object repository {
+
+  type DBTransactor = Has[DBTransactor.Resource]
+
+  object DBTransactor {
+
+    trait Resource {
+
+      val xa: Transactor[Task]
+
+    }
+
+    val live: ZLayer[Has[DatabaseConfig], Nothing, DBTransactor] =
+      ZLayer.fromFunction { env =>
+        val cfg = env.get[DatabaseConfig]
+        new Resource {
+          override val xa: Transactor[Task] = Transactor.fromDriverManager(cfg.driver, cfg.url, cfg.user, cfg.password)
+        }
+      }
+
+    val xa: ZIO[DBTransactor, Nothing, Transactor[Task]] = ZIO.access[DBTransactor](_.get[Resource].xa)
+
+  }
+
+}
