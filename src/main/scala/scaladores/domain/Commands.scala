bc27930ff954c1d2f16402586fd968d599fa5b96
J. Eduardo V. Morango <edvmorango@gmail.com>
2020-04-21 21:29:00 -0300

    feat: event definitions

--- src/main/scala/scaladores/domain/Commands.scala
+++ src/main/scala/scaladores/domain/Commands.scala
@@ -1,5 +1,14 @@
 package scaladores.domain
 
 import java.util.UUID
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto._
 
 case class CreateAccountCommand(correlationUuid: UUID, document: String)
+
+object CreateAccountCommand {
+
+  implicit val CreateAccountCommandEncoder: Encoder[CreateAccountCommand] = deriveEncoder[CreateAccountCommand]
+  implicit val CreateAccountCommandDecoder: Decoder[CreateAccountCommand] = deriveDecoder[CreateAccountCommand]
+
+}
---
+++ src/main/scala/scaladores/domain/Events.scala
@@ -0,0 +1,41 @@
+package scaladores.domain
+
+import java.time.OffsetDateTime
+import java.util.UUID
+
+import io.circe.{Decoder, Encoder}
+import io.circe.generic.semiauto._
+import io.circe.syntax._
+import cats.syntax.functor._
+
+case class Event(uuid: UUID,
+                 correlationUuid: UUID,
+                 serial: Long,
+                 accountUuid: UUID,
+                 createdAt: OffsetDateTime,
+                 body: EventContent)
+
+object Event {
+
+  implicit val EventEncoder: Encoder[Event] = deriveEncoder[Event]
+  implicit val EventDecoder: Decoder[Event] = deriveDecoder[Event]
+
+}
+
+sealed trait EventContent
+
+object EventContent {
+
+  case class AccountCreatedEvent(account: Account) extends EventContent
+
+  implicit val AccountCreatedEventEncoder: Encoder[AccountCreatedEvent] = deriveEncoder[AccountCreatedEvent]
+  implicit val AccountCreatedEventDecoder: Decoder[AccountCreatedEvent] = deriveDecoder[AccountCreatedEvent]
+
+  implicit val EventContentEncoder: Encoder[EventContent] = Encoder.instance[EventContent] {
+    case e: AccountCreatedEvent => e.asJson
+  }
+
+  implicit val EventContentDecoder: Decoder[EventContent] =
+    List[Decoder[EventContent]](AccountCreatedEventDecoder.widen).reduceLeft(_ or _)
+
+}
