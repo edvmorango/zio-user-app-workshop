2f7f8cb84c5a0fcc948e40dfe45c55158a061b5e
J. Eduardo V. Morango <edvmorango@gmail.com>
2020-04-22 02:41:05 -0300

    reinit

--- src/main/scala/scaladores/app/Main.scala
+++ src/main/scala/scaladores/app/Main.scala
@@ -1,22 +1,9 @@
 package scaladores.app
 
-import scaladores.consumer.AccountConsumer
-import scaladores.endpoint.Server
-import scaladores.environment.Environments._
-import scaladores.environment.config.Layers._
 import zio.{App, ZIO}
 
 object Main extends App {
 
-  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = {
-
-    val pipeline: ZIO[Any, Nothing, Int] = for {
-      _ <- AccountConsumer.pipeline.provideLayer(appEnvironment).fork
-      _ <- Server.runServer.provideLayer(httpServer ++ appEnvironment)
-    } yield 1
-
-    pipeline
-
-  }
+  override def run(args: List[String]): ZIO[zio.ZEnv, Nothing, Int] = ZIO.succeed(1)
 
 }
--- src/main/scala/scaladores/failure/EventFailure.scala
+++
@@ -1,9 +0,0 @@
-package scaladores.failure
-
-//import io.circe.Encoder
-//import io.circe.generic.semiauto._
-//import io.circe.syntax._
-
-sealed trait EventFailure
-
-object EventFailure {}
